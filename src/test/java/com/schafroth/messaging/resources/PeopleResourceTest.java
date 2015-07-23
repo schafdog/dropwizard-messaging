a1d72cc2220f1d5a2a30143f27939a6aaab24a4c
Dennis Schafroth <dennis@schafroth.dk>
2015-07-24 01:02:29 +0200

    Add more mock to list test to work. Drop verify on non-mock object

--- src/test/java/com/schafroth/messaging/resources/PeopleResourceTest.java
+++ src/test/java/com/schafroth/messaging/resources/PeopleResourceTest.java
@@ -1,14 +1,14 @@
 package com.schafroth.messaging.resources;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import io.dropwizard.testing.junit.ResourceTestRule;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
@@ -26,13 +26,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.ScanResult;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableList;
-import com.schafroth.messaging.resources.JsonResource;
 
 /**
  * Unit tests for {@link JsonResource}.
@@ -42,21 +42,24 @@ public class PeopleResourceTest {
     private static final JedisPool pool = mock(JedisPool.class);
     private static final Jedis jedis = mock(Jedis.class);
     private static final JsonResource jsonResource = new JsonResource(pool);
-    @ClassRule
+	@SuppressWarnings("unchecked")
+	private static final ScanResult<String> scanResult = (ScanResult<String>) mock(ScanResult.class);
+
+	@ClassRule
     public static final ResourceTestRule resources = ResourceTestRule.builder()
             .addResource(jsonResource)
             .build();
     @Captor
     private ArgumentCaptor<JsonNode> messageCaptor;
     private JsonNode message;
-    String uuid = UUID.randomUUID().toString();
-    String payload = UUID.randomUUID().toString();;
+    String uuid = "1";
+    String payload = "payload";
     @Before
     public void setUp() {
         ObjectMapper m = new ObjectMapper();
         message = m.createObjectNode();
         ((ObjectNode) message).put("UUID", uuid);
-        ((ObjectNode) message).put("message", payload);
+        ((ObjectNode) message).put("payload", payload);
     }
 
     @After
@@ -67,26 +70,32 @@ public class PeopleResourceTest {
     @Test
     public void create() throws JsonProcessingException {
     	String channel = "channel";
-    	String stringmmsg = message.asText();
+    	String stringMsg = message.toString();
         when(pool.getResource()).thenReturn(jedis);
-        when(jedis.publish(channel, stringmmsg)).thenReturn(1l);
+        when(jedis.publish(channel, stringMsg)).thenReturn(1l);
         final Response response = resources.client().target("/message")
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .post(Entity.entity(message, MediaType.APPLICATION_JSON_TYPE));
 
         assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
-        verify(jsonResource).create(messageCaptor.capture());
-        assertThat(messageCaptor.getValue()).isEqualTo(message);
     }
 
     @Test
     public void list() throws Exception {
-        final ImmutableList<JsonNode> messageList = ImmutableList.of(message);
+    	String stringMsg = message.toString();
+        when(pool.getResource()).thenReturn(jedis);
+    	List<String> stringList = new LinkedList<String>();
+    	stringList.add(stringMsg);
+    	when(jedis.scan("0")).thenReturn(scanResult);
+    	when(scanResult.getStringCursor()).thenReturn("0");
+    	when(scanResult.getResult()).thenReturn(stringList);
+    	when(jedis.get(anyString())).thenReturn(stringMsg);
+
+    	final ImmutableList<JsonNode> messageList = ImmutableList.of(message);
 
         final List<JsonNode> response = resources.client().target("/message")
                 .request().get(new GenericType<List<JsonNode>>() {});
 
-        verify(jsonResource).list();
         assertThat(response).containsAll(messageList);
     }
 }
