7d86fdc4f138aace1d1898b2dea04da3d17f613a
Dennis Schafroth <dennis@schafroth.dk>
2015-07-23 14:37:02 +0200

    Refactor to message API, and jedis publish

--- src/test/java/com/example/helloworld/resources/PeopleResourceTest.java
+++ src/test/java/com/example/helloworld/resources/PeopleResourceTest.java
@@ -1,10 +1,20 @@
 package com.example.helloworld.resources;
 
-import com.example.helloworld.core.Person;
-import com.example.helloworld.db.PersonDAO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.common.collect.ImmutableList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import io.dropwizard.testing.junit.ResourceTestRule;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -14,66 +24,68 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
 
 /**
- * Unit tests for {@link PeopleResource}.
+ * Unit tests for {@link JsonResource}.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class PeopleResourceTest {
-    private static final PersonDAO dao = mock(PersonDAO.class);
+    private static final JedisPool pool = mock(JedisPool.class);
+    private static final Jedis jedis = mock(Jedis.class);
+    private static final JsonResource jsonResource = new JsonResource(pool);
     @ClassRule
     public static final ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new PeopleResource(dao))
+            .addResource(jsonResource)
             .build();
     @Captor
-    private ArgumentCaptor<Person> personCaptor;
-    private Person person;
-
+    private ArgumentCaptor<JsonNode> messageCaptor;
+    private JsonNode message;
+    String uuid = UUID.randomUUID().toString();
+    String payload = UUID.randomUUID().toString();;
     @Before
     public void setUp() {
-        person = new Person();
-        person.setFullName("Full Name");
-        person.setJobTitle("Job Title");
+        ObjectMapper m = new ObjectMapper();
+        message = m.createObjectNode();
+        ((ObjectNode) message).put("UUID", uuid);
+        ((ObjectNode) message).put("message", payload);
     }
 
     @After
     public void tearDown() {
-        reset(dao);
+        reset(pool);
     }
 
     @Test
-    public void createPerson() throws JsonProcessingException {
-        when(dao.create(any(Person.class))).thenReturn(person);
-        final Response response = resources.client().target("/people")
+    public void create() throws JsonProcessingException {
+    	String channel = "channel";
+    	String stringmmsg = message.asText();
+        when(pool.getResource()).thenReturn(jedis);
+        when(jedis.publish(channel, stringmmsg)).thenReturn(1l);
+        final Response response = resources.client().target("/message")
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .post(Entity.entity(person, MediaType.APPLICATION_JSON_TYPE));
+                .post(Entity.entity(message, MediaType.APPLICATION_JSON_TYPE));
 
         assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
-        verify(dao).create(personCaptor.capture());
-        assertThat(personCaptor.getValue()).isEqualTo(person);
+        verify(jsonResource).create(messageCaptor.capture());
+        assertThat(messageCaptor.getValue()).isEqualTo(message);
     }
 
     @Test
-    public void listPeople() throws Exception {
-        final ImmutableList<Person> people = ImmutableList.of(person);
-        when(dao.findAll()).thenReturn(people);
+    public void list() throws Exception {
+        final ImmutableList<JsonNode> messageList = ImmutableList.of(message);
 
-        final List<Person> response = resources.client().target("/people")
-                .request().get(new GenericType<List<Person>>() {});
+        final List<JsonNode> response = resources.client().target("/message")
+                .request().get(new GenericType<List<JsonNode>>() {});
 
-        verify(dao).findAll();
-        assertThat(response).containsAll(people);
+        verify(jsonResource).list();
+        assertThat(response).containsAll(messageList);
     }
 }
