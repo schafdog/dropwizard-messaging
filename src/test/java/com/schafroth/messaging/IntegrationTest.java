bedb8633a748351c49d64f73d0f86fd2b2055503
Dennis Schafroth <dennis@schafroth.dk>
2015-07-27 14:39:03 +0200

    Remove DB migration

--- src/test/java/com/schafroth/messaging/IntegrationTest.java
+++ src/test/java/com/schafroth/messaging/IntegrationTest.java
@@ -1,13 +1,8 @@
 package com.schafroth.messaging;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -21,26 +16,21 @@ import org.junit.ClassRule;
 import org.junit.Test;
 
 import com.google.common.base.Optional;
-import com.schafroth.messaging.MessagingApplication;
-import com.schafroth.messaging.MessagingConfiguration;
 import com.schafroth.messaging.core.Message;
 import com.schafroth.messaging.core.Saying;
 
 public class IntegrationTest {
 
-    private static final String TMP_FILE = createTempFile();
     private static final String CONFIG_PATH = ResourceHelpers.resourceFilePath("test-example.yml");
 
     @ClassRule
     public static final DropwizardAppRule<MessagingConfiguration> RULE = new DropwizardAppRule<>(
-            MessagingApplication.class, CONFIG_PATH,
-            ConfigOverride.config("database.url", "jdbc:h2:" + TMP_FILE));
+            MessagingApplication.class, CONFIG_PATH);
 
     private Client client;
 
     @BeforeClass
-    public static void migrateDb() throws Exception {
-        RULE.getApplication().run("db", "migrate", CONFIG_PATH);
+    public static void beforeClass() throws Exception {
     }
 
     @Before
@@ -52,14 +42,6 @@ public class IntegrationTest {
     public void tearDown() throws Exception {
         client.close();
         
-    }
-
-    private static String createTempFile() {
-        try {
-            return File.createTempFile("test-example", null).getAbsolutePath();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
     }
 
     @Test
