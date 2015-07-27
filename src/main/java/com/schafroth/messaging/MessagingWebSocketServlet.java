6a6998561f76f53d0a79de0dc4bcf0e49a02b78d
Dennis Schafroth <dennis@schafroth.dk>
2015-07-27 14:46:13 +0200

    Use custom web socket creator

--- src/main/java/com/schafroth/messaging/MessagingWebSocketServlet.java
+++ src/main/java/com/schafroth/messaging/MessagingWebSocketServlet.java
@@ -3,16 +3,25 @@ package com.schafroth.messaging;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+import redis.clients.jedis.JedisPool;
+
 public class MessagingWebSocketServlet extends WebSocketServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -9152808383213487609L;
-
+	private JedisPool pool;
+	
+	public MessagingWebSocketServlet(JedisPool pool) {
+		this.pool = pool;
+	}
+	
+	
 	@Override
 	public void configure(WebSocketServletFactory factory) {
-		factory.register(MessagingWebSocketEndpoint.class);
+		//factory.register(MessagingWebSocketEndpoint.class);		
+		factory.setCreator(new JedisPoolWebSocketCreator(pool));
 	}
 	
 	
